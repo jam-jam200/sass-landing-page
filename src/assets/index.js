4c95123c5642165ad8c0f698c133496e199a7404
jam-jam200 <jessieoyaks4j@gmail.com>
2022-11-28 23:15:11 +0100

    starting

---
+++ src/assets/index.js
@@ -0,0 +1,55 @@
+import arshakirsvg from "./AR Shakir.svg";
+import arrow from "./arrow.png";
+import blockRight from "./blockRight.png";
+import blockLeft from "./blocks.png";
+import cookies from "./cookies.png";
+import curlyArrow from "./curlyArrow.png";
+import followerImg from "./follwerImg.png";
+import headerImg from "./headerImg.png";
+import line from "./line.png";
+import loginArrow from "./loginArrow.png";
+import logo from "./logo.png";
+import minus from "./minus.png";
+import monday from "./monday.png";
+import money from "./money.png";
+import morpheus from "./morpheus.png";
+import openZeppelin from "./openZeppelin.png";
+import people from "./people.png";
+import plus from "./plus.png";
+import protonet from "./protonet.png";
+import purpleArrow from "./purpleArrow.png";
+import quote from "./quote.png";
+import samsung from "./samsung.png";
+import security from "./security.png";
+import segment from "./segment.png";
+import socialmediaImg from "./socialmediaImg.png";
+import subscribeImg from "./suscribeImg";
+
+export {
+  arshakirsvg,
+  arrow,
+  blockLeft,
+  blockRight,
+  cookies,
+  curlyArrow,
+  followerImg,
+  headerImg,
+  line,
+  loginArrow,
+  logo,
+  minus,
+  monday,
+  money,
+  morpheus,
+  openZeppelin,
+  people,
+  plus,
+  protonet,
+  purpleArrow,
+  quote,
+  samsung,
+  security,
+  segment,
+  socialmediaImg,
+  subscribeImg,
+};
