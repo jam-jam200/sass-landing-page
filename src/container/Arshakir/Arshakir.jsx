49e1ec2f7e3aa2319a03e5a45161a60e0b05293a
jam-jam200 <jessieoyaks4j@gmail.com>
2022-11-29 13:35:24 +0100

    packages section

--- src/container/Arshakir/Arshakir.jsx
+++ src/container/Arshakir/Arshakir.jsx
@@ -1,6 +1,20 @@
 import React from "react";
 import "./Arshakir.scss";
 
+const Feature = ({ title, text }) => {
+  return (
+    <div className="gpt3__features-container__feature">
+      <div className="gpt3__features-container__feature-title">
+        <div />
+        <h1>{title}</h1>
+      </div>
+      <div className="gpt3__features-container_feature-text">
+        <p>{text}</p>
+      </div>
+    </div>
+  );
+};
+
 const Ashakir = () => {
   return <div>Ashakir</div>;
 };
