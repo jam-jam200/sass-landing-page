335bad4f0b1c3bcdcceee67479d31376acbdb21f
jam-jam200 <jessieoyaks4j@gmail.com>
2022-11-29 22:29:48 +0100

    products

--- src/container/Followers/Followers.jsx
+++ src/container/Followers/Followers.jsx
@@ -1,8 +1,37 @@
 import React from "react";
-import "./Followers.css";
+import { people, followerImg, arrow } from "../../assets";
+import "./Followers.scss";
 
-const Followers = () => {
-  return <div>Followers</div>;
-};
+const Followers = () => (
+  <div className="arshakir__followers section__padding" id="products">
+    <div className="arshakir__followers-content">
+      <h1 className="heading__text">Get thousands of active followers</h1>
+      <p>
+        Yet bed any for travelling assistance indulgence unpleasing. Not
+        thoughts all exercise blessing. Indulgence way everything joy alteration
+        boisterous the attachment.
+      </p>
+
+      <div className="arshakir__followers-content__input">
+        <button type="button">
+          Get Started
+          <img src={arrow} alt="arrow" />
+        </button>
+      </div>
+
+      <div className="arshakir__followers-content__people">
+        <img src={people} />
+        <p>
+          12,000+ people already joined the arshakir&apos;s <br /> increase
+          followers plan. Get started today!
+        </p>
+      </div>
+    </div>
+
+    <div className="arshakir__followers-image">
+      <img src={followerImg} />
+    </div>
+  </div>
+);
 
 export default Followers;
