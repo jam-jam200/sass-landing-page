eb5498c4a1293e83cd9ff82d8d89c5e350f8a3dc
jam-jam200 <jessieoyaks4j@gmail.com>
2022-12-05 09:03:15 +0100

    new week

--- src/container/SocialMedia/SocialMedia.jsx
+++ src/container/SocialMedia/SocialMedia.jsx
@@ -1,11 +1,16 @@
 import React from "react";
-import { followerImg } from "../../assets";
+import { socialmediaImg } from "../../assets";
 import "./SocialMedia.scss";
 
 const SocialMedia = () => (
   <div className="arshakir__socialmedia section__margin section__padding">
+    <div className="arshakir__socialmedia-image">
+      <img src={socialmediaImg} />
+    </div>
     <div className="arshakir__socialmedia-content">
-      <h1 className="heading__text">Daily active users <br /> on social media</h1>
+      <h1 className="heading__text">
+        Daily active users <br /> on social media
+      </h1>
       <p>
         Yet bed any for travelling assistance indulgence unpleasing. Not
         thoughts all exercise blessing. Indulgence way everything joy alteration
@@ -15,10 +20,6 @@ const SocialMedia = () => (
       <div className="arshakir__socialmedia-content__input">
         <button type="button">Start Free</button>
       </div>
-    </div>
-
-    <div className="arshakir__socialmedia-image">
-      <img src={followerImg} />
     </div>
   </div>
 );
