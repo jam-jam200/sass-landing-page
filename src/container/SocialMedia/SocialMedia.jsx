fec3fdaa52603410deb2410da693b7a5fdcbd4e4
jam-jam200 <jessieoyaks4j@gmail.com>
2022-12-06 14:48:19 +0100

    socialmedia

--- src/container/SocialMedia/SocialMedia.jsx
+++ src/container/SocialMedia/SocialMedia.jsx
@@ -1,27 +1,28 @@
 import React from "react";
-import { socialmediaImg } from "../../assets";
 import "./SocialMedia.scss";
+import { socialmediaImg } from "../../assets";
 
-const SocialMedia = () => (
-  <div className="arshakir__socialmedia section__margin section__padding">
-    <div className="arshakir__socialmedia-image">
-      <img src={socialmediaImg} />
-    </div>
-    <div className="arshakir__socialmedia-content">
-      <h1 className="heading__text">
-        Daily active users <br /> on social media
-      </h1>
-      <p>
-        Yet bed any for travelling assistance indulgence unpleasing. Not
-        thoughts all exercise blessing. Indulgence way everything joy alteration
-        boisterous the attachment.
-      </p>
-
-      <div className="arshakir__socialmedia-content__input">
-        <button type="button">Start Free</button>
+const SocialMedia = () => {
+  return (
+    <div className="arshakir__socialmedia section__padding" id="possibility">
+      <div className="arshakir__socialmedia-image">
+        <img src={socialmediaImg} alt="possibility" />
+      </div>
+      <div className="arshakir__socialmedia-content">
+        <h4>Request Early Access to Get Started</h4>
+        <h1 className="gradient__text">
+          The possibilities are beyond your imagination
+        </h1>
+        <p>
+          Yet bed any for travelling assistance indulgence unpleasing. Not
+          thoughts all exercise blessing. Indulgence way everything joy
+          alteration boisterous the attachment. Party we years to order allow
+          asked of.
+        </p>
+        <h4>Request Early Access to Get Started</h4>
       </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default SocialMedia;
