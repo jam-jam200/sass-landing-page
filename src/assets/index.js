2988188e2c923a8e2c9a98bb4e72af5c00ae2dbf
jam-jam200 <jessieoyaks4j@gmail.com>
2022-12-06 16:47:36 +0100

    socialmedia

--- src/assets/index.js
+++ src/assets/index.js
@@ -17,6 +17,9 @@ import morpheus from "./morpheus.png";
 import openZeppelin from "./openZeppelin.png";
 import oracle from "./oracle.png";
 import people from "./people.png";
+import people01 from "./people01.png";
+import people02 from "./people01.png";
+import people03 from "./people03.png";
 import plus from "./plus.png";
 import protonet from "./protonet.png";
 import purpleArrow from "./purpleArrow.png";
@@ -24,6 +27,7 @@ import quote from "./quote.png";
 import samsung from "./samsung.png";
 import security from "./security.png";
 import segment from "./segment.png";
+import star from "./stars.png";
 import socialmediaImg from "./socialmediaImg.png";
 import subscribeImg from "./subscribeImg.png";
 
@@ -47,6 +51,9 @@ export {
   oracle,
   openZeppelin,
   people,
+  people01,
+  people02,
+  people03,
   plus,
   protonet,
   purpleArrow,
@@ -54,6 +61,7 @@ export {
   samsung,
   security,
   segment,
+  star,
   socialmediaImg,
   subscribeImg,
 };
--- src/container/Testimonials/Testimonials.jsx
+++ src/container/Testimonials/Testimonials.jsx
@@ -1,8 +1,36 @@
 import React from "react";
-import "./Testimonials.css";
+import "./Testimonials.scss";
+import { people01, people02, people03, star } from "../../assets";
+
+const testimonials = [
+  {
+    id: "feedback-1",
+    content:
+      "On the other hand, we denounce with righteous indignation and dislike men who are so beguiled and.",
+    name: "Serhiy Hipskyy",
+    title: "CEO Universal",
+    img: people01,
+  },
+  {
+    id: "feedback-2",
+    content:
+      "Cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus.",
+    name: "Justus Menke",
+    title: "CEO Eronaman",
+    img: people02,
+  },
+  {
+    id: "feedback-3",
+    content:
+      "Cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus.",
+    name: "Justus Menke",
+    title: "CEO Eronaman",
+    img: people03,
+  },
+];
 
 const Testimonials = () => {
-  return <div>Testimonials</div>;
+  return <div className="section__padding arshakir__testimonials">Testimonials</div>;
 };
 
 export default Testimonials;
