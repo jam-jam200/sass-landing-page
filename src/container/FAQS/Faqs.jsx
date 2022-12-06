9bcc98e9136794ff20a4f433415401219154166a
jam-jam200 <jessieoyaks4j@gmail.com>
2022-12-07 00:53:56 +0100

    faqs

--- src/container/FAQS/Faqs.jsx
+++ src/container/FAQS/Faqs.jsx
@@ -1,8 +1,31 @@
 import React from "react";
-import "./Faqs.css";
+import { followerImg, purpleArrow } from "../../assets";
+import "./Faqs.scss";
 
-const Faqs = () => {
-  return <div>faqs</div>;
-};
+const Faqs = () => (
+  <div className="arshakir__faqs section__padding" id="products">
+    <div className="arshakir__faqs-content">
+      <h1 className="heading__text">
+        Any questions? <br /> We got you.
+      </h1>
+      <p>
+        Yet bed any for travelling assistance indulgence unpleasing. Not
+        thoughts all exercise blessing. Indulgence way everything joy alteration
+        boisterous the attachment.
+      </p>
+
+      <div className="arshakir__header-content__input">
+        <button type="button">
+          More FAQs
+          <img src={purpleArrow} alt="arrow" />
+        </button>
+      </div>
+    </div>
+
+    <div className="arshakir__faqs-image">
+      <img src={followerImg} />
+    </div>
+  </div>
+);
 
 export default Faqs;
