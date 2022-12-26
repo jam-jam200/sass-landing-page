bed348986a35e6da0a8e95482c439f9c6c921dab
jam-jam200 <jessieoyaks4j@gmail.com>
2022-12-26 15:00:01 +0100

    faq questions using accordions

--- src/components/Accordions/Accordion.jsx
+++ src/components/Accordions/Accordion.jsx
@@ -1,35 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Accordion.scss";
-
-const faq__questions = [
-  {
-    question: "How this work?",
-    answer:
-      "Yet bed any for  assistance indulgence unpleasing. Not thoughts all exercise blessing. Indulgence way everything joy alteration boisterous the attachment.",
-  },
-  {
-    question: "Are there any additional fee?",
-    answer:
-      "Yet bed any for  assistance indulgence unpleasing. Not thoughts all exercise blessing. Indulgence way everything joy alteration boisterous the attachment.",
-  },
-  {
-    question: "How can I get the app?",
-    answer:
-      "Yet bed any for  assistance indulgence unpleasing. Not thoughts all exercise blessing. Indulgence way everything joy alteration boisterous the attachment.",
-  },
-  {
-    question: "What features do you offer and other not?",
-    answer:
-      "Yet bed any for  assistance indulgence unpleasing. Not thoughts all exercise blessing. Indulgence way everything joy alteration boisterous the attachment.",
-  },
-];
+import { AiOutlineMinus, AiOutlinePlus } from "react-icons/ai";
+import { faq__questions } from "../../constants";
 
 const Accordion = () => {
+  const [accordion, setAccordion] = useState(-1);
+
+  const handleAccordion = (index) => {
+    setAccordion(index);
+  };
+
   return (
-    <div className="arshakir__accodion__container">
+    <div className="arshakir__accordion__container">
       <div className="arshakir__accordion-questions">
         {faq__questions.map((question, index) => (
-          <div key={index+question.question}></div>
+          <div
+            key={index + question.question}
+            onClick={() => handleAccordion(index)}
+          >
+            <div className="arshakir__accordion__question-heading">
+              <h3 className={accordion === index ? "active" : ""}>
+                {question.question}
+              </h3>
+            </div>
+            <div className="arshakir__accordion-minus">
+              {accordion === index ? <AiOutlineMinus /> : <AiOutlinePlus />}
+            </div>
+            <div>
+              <p>{question.answer}</p>
+            </div>
+          </div>
         ))}
       </div>
     </div>
---
+++ src/constants/index.js
@@ -0,0 +1,22 @@
+export const faq__questions = [
+  {
+    question: "How this work?",
+    answer:
+      "Yet bed any for  assistance indulgence unpleasing. Not thoughts all exercise blessing. Indulgence way everything joy alteration boisterous the attachment.",
+  },
+  {
+    question: "Are there any additional fee?",
+    answer:
+      "Yet bed any for  assistance indulgence unpleasing. Not thoughts all exercise blessing. Indulgence way everything joy alteration boisterous the attachment.",
+  },
+  {
+    question: "How can I get the app?",
+    answer:
+      "Yet bed any for  assistance indulgence unpleasing. Not thoughts all exercise blessing. Indulgence way everything joy alteration boisterous the attachment.",
+  },
+  {
+    question: "What features do you offer and other not?",
+    answer:
+      "Yet bed any for  assistance indulgence unpleasing. Not thoughts all exercise blessing. Indulgence way everything joy alteration boisterous the attachment.",
+  },
+];
