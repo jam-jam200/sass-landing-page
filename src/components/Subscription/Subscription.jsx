813c5f3ec42b9266743103ebf5d1d34ca4878e30
jam-jam200 <jessieoyaks4j@gmail.com>
2023-01-04 15:16:24 +0100

    subscription section in progress

--- src/components/Subscription/Subscription.jsx
+++ src/components/Subscription/Subscription.jsx
@@ -2,7 +2,19 @@ import React from "react";
 import "./Subscription.scss";
 
 const Subscription = () => {
-  return <div>subcription</div>;
+  return (
+    <div className="arshakir__subscribe section__padding" id="products">
+      <div className="arshakir__subscribe-content">
+        <p className="heading__text">Join our community</p>
+        <h3>Set your social media growth on autopilot</h3>
+
+        <div className="arshakir__subscribe-content__input">
+          <input type="email" placeholder="Your Email Address" />
+          <button type="button">Subscribe</button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Subscription;
