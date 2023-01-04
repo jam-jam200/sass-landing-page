87a18e7cf157a7b942a0aa3ea73a6f73256dccba
jam-jam200 <jessieoyaks4j@gmail.com>
2023-01-04 11:35:51 +0100

    accordions

--- src/components/Accordions/Accordion.jsx
+++ src/components/Accordions/Accordion.jsx
@@ -1,53 +1,39 @@
 import React, { useState } from "react";
 import "./Accordion.scss";
 import { AiOutlineMinus, AiOutlinePlus } from "react-icons/ai";
-import { faq__questions } from "../../constants";
 import Accordion from "react-bootstrap/Accordion";
 
 const Accordions = () => {
-  const [activeAccordion, setActiveAccordion] = useState(null);
+  const [activeAccordion, setActiveAccordion] = useState([
+    [
+      {
+        question: "How does this work?",
+        answer:
+          "Yet bed any for  assistance indulgence unpleasing. Not thoughts all exercise blessing. Indulgence way everything joy alteration boisterous the attachment.",
+        open: true,
+      },
+      {
+        question: "Are there any additional fee?",
+        answer:
+          "Yet bed any for  assistance indulgence unpleasing. Not thoughts all exercise blessing. Indulgence way everything joy alteration boisterous the attachment.",
+        open: false,
+      },
+      {
+        question: "How can I get the app?",
+        answer:
+          "Yet bed any for  assistance indulgence unpleasing. Not thoughts all exercise blessing. Indulgence way everything joy alteration boisterous the attachment.",
+        open: false,
+      },
+      {
+        question: "What features do you offer and what don't you?",
+        answer:
+          "Yet bed any for  assistance indulgence unpleasing. Not thoughts all exercise blessing. Indulgence way everything joy alteration boisterous the attachment.",
+        open: false,
+      },
+    ],
+  ]);
 
-  return (
-    <div className="arshakir__accordion__container wrapper">
-      {faq__questions.map((item, index) => (
-        <Accordion defaultActiveKey="0">
-          <Accordion.Item eventKey={item.id}>
-            <Accordion.Header>{item.question}</Accordion.Header>
-            <Accordion.Body>{item.answer}</Accordion.Body>
-          </Accordion.Item>
-        </Accordion>
-      ))}
-      {/* <div className="arshakir__accordion-questions accordion">
-        {faq__questions.map((question, index) => (
-          <div key={index} className="arshakir__accordion-question item">
-            <div
-              className="arshakir__accordion__question-heading title"
-              
-            >
-              <h3>{question.question}</h3>
-              <span>
-                {" "}
-                {activeAccordion === index ? (
-                  <AiOutlineMinus color="#000" />
-                ) : (
-                  <AiOutlinePlus color="#000" />
-                )}
-              </span>
-            </div>
-            <div
-              className={
-                activeAccordion === index
-                  ? "arshakir__accordion__answer-content show"
-                  : "arshakir__accordion__answer-content"
-              }
-            >
-              {question.answer}
-            </div>
-          </div>
-        ))}
-      </div> */}
-    </div>
-  );
+  return <div className="arshakir__accordion__container wrapper"></div>;
 };
 
 export default Accordions;
