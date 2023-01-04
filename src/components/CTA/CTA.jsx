1ccebc02dae4163beb9eb22e535a35f1c8a8e37f
jam-jam200 <jessieoyaks4j@gmail.com>
2023-01-04 22:22:59 +0100

    working on CTA

--- src/components/CTA/CTA.jsx
+++ src/components/CTA/CTA.jsx
@@ -1,8 +1,24 @@
 import React from "react";
 import "./CTA.scss";
+import { arrow } from "../../assets";
 
 const CTA = () => {
-  return <div>cta</div>;
+  return (
+    <div className="section__padding arshakir__CTA">
+      <div className="arshakir__CTA-heading">
+        <h1>
+          Join millions of <br /> creative people with Shakir
+        </h1>
+
+        <div className="arshakir__CTA-content__input">
+          <button type="button">
+            Join the waitlist
+            <img src={arrow} alt="arrow" />
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default CTA;
