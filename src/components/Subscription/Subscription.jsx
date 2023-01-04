dcd681217ba610b1462c8299f62bd57c8d0fac95
jam-jam200 <jessieoyaks4j@gmail.com>
2023-01-04 15:25:15 +0100

    subscription section in progress

--- src/components/Subscription/Subscription.jsx
+++ src/components/Subscription/Subscription.jsx
@@ -5,8 +5,10 @@ const Subscription = () => {
   return (
     <div className="arshakir__subscribe section__padding" id="products">
       <div className="arshakir__subscribe-content">
-        <p className="heading__text">Join our community</p>
-        <h3>Set your social media growth on autopilot</h3>
+        <p>Join our community</p>
+        <h3>
+          Set your social media <br /> growth on autopilot
+        </h3>
 
         <div className="arshakir__subscribe-content__input">
           <input type="email" placeholder="Your Email Address" />
