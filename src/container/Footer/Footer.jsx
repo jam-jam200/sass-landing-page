cefa2f4c9fa4b152b1c13bcc8ddf51f73349252d
jam-jam200 <jessieoyaks4j@gmail.com>
2023-01-06 14:07:55 +0100

    working on the footer links

--- src/container/Footer/Footer.jsx
+++ src/container/Footer/Footer.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "./Footer.scss";
-import { company, product, resources } from "../../constants";
+import { company, product, resources, footerLinks } from "../../constants";
 import { logosvg } from "../../assets";
 
 const Footer = () => {
@@ -17,7 +17,25 @@ const Footer = () => {
           </p>
         </div>
         <div className="arshakir__footer-links_div">
-          <h4>Company</h4>
+          {footerLinks.map((footerlink) => (
+            <div key={footerlink.title}>
+              <h4>{footerlink.title}</h4>
+              <ul>
+                {footerlink.links.map((links, index) => (
+                  <li
+                    key={links.name}
+                    className={`${
+                      index !== footerlink.links.length - 1 ? "mb-4" : "mb-0"
+                    }`}
+                  >
+                    {links.name}
+                  </li>
+                ))}
+              </ul>
+            </div>
+          ))}
+
+          {/* <h4>Company</h4>
           {company.map((companys, index) => (
             <p key={index + companys}>{companys.name}</p>
           ))}
@@ -32,7 +50,7 @@ const Footer = () => {
           <h4>Resources</h4>
           {resources.map((resource, index) => (
             <p key={index + resource}>{resource.name}</p>
-          ))}
+          ))} */}
         </div>
       </div>
     </div>
