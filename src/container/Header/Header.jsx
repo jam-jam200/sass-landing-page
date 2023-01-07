82f9f002a86a370c553ef90ddab3d4960fe9f02b
jam-jam200 <jessieoyaks4j@gmail.com>
2023-01-07 12:52:21 +0100

    little adjustment on the header section

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -5,10 +5,12 @@ import "./Header.scss";
 const Header = () => (
   <div className="arshakir__header section__padding">
     <div className="arshakir__header-content">
-      <h1 className="heading__text">Increase your followers on social media</h1>
+      <h1 className="heading__text">
+        Increase your <br /> followers on <br /> social media
+      </h1>
       <p>
-        Yet bed any for travelling assistance indulgence unpleasing. Not
-        thoughts all exercise blessing. Indulgence way everything joy alteration
+        Yet bed any for travelling assistance indulgence unpleasing. <br /> Not
+        thoughts all exercise blessing. Indulgence way <br /> everything joy alteration
         boisterous the attachment.
       </p>
 
@@ -22,8 +24,8 @@ const Header = () => (
       <div className="arshakir__header-content__people">
         <img src={people} />
         <p>
-          12,000+ people already joined the arshakir&apos;s <br /> increase followers
-          plan. Get started today!
+          12,000+ people already joined the arshakir&apos;s <br /> increase
+          followers plan. Get started today!
         </p>
       </div>
     </div>
